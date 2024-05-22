d73bb30f4f1572b856dee3cef6b962695410aa5f
sato-kouki <rstoyo3.sato.koki@techcamp-training.com>
2024-05-22 17:57:32 +0900

    Isseu 4~6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -22,6 +22,10 @@ class CalendarsController < ApplicationController
     wdays = ['日','月','火','水','木','金','土'] # 曜日の文字列配列
     
     @todays_date = Date.today
+<<<<<<< Updated upstream
+=======
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+>>>>>>> Stashed changes
     @week_days = []
   
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -31,9 +35,14 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< Updated upstream
       # 曜日の文字列を取得し、日付と一緒に表示する
       day_of_week = wdays[(@todays_date + x).wday]
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :day_of_week => day_of_week, :plans => today_plans }
+=======
+      day_of_week = wdays[(@todays_date + x).wday]
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day,:day_of_week => day_of_week, :plans => today_plans}
+>>>>>>> Stashed changes
       @week_days.push(days)
     end
   end
