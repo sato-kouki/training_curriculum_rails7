47aca01205f554ab38b222d06ec648c4781f0015
sato-kouki <rstoyo3.sato.koki@techcamp-training.com>
2024-05-22 18:01:49 +0900

    Isseu 4~6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,35 +15,28 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
-    wdays = ['日','月','火','水','木','金','土'] # 曜日の文字列配列
-    
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-<<<<<<< Updated upstream
-=======
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
->>>>>>> Stashed changes
+
     @week_days = []
-  
+
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-  
+
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< Updated upstream
-      # 曜日の文字列を取得し、日付と一緒に表示する
-      day_of_week = wdays[(@todays_date + x).wday]
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :day_of_week => day_of_week, :plans => today_plans }
-=======
-      day_of_week = wdays[(@todays_date + x).wday]
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day,:day_of_week => day_of_week, :plans => today_plans}
->>>>>>> Stashed changes
+      days = { month:(@todays_date + x).month, date:(@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
+
   end
 end
