0d4d218371e68980cc515fbf8ebf1e13c6108de5
sato-kouki <rstoyo3.sato.koki@techcamp-training.com>
2024-05-23 14:51:21 +0900

    ロケットハッシュの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -33,7 +33,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       day_of_week = wdays[(@todays_date + x).wday]
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day,:day_of_week => day_of_week, :plans => today_plans}
+      days = { month:(@todays_date + x).month, date:(@todays_date+x).day, day_of_week:day_of_week, plans:today_plans}
       @week_days.push(days)
     end
   end
